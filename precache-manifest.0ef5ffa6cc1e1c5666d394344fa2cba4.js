7795ddfda17de19fabb2bb9f0c7e649456bbe8b5
Belga-JS <Meherzi.belga@yahoo.com>
2019-12-19 01:48:01 +0100

    Updates

---
+++ precache-manifest.0ef5ffa6cc1e1c5666d394344fa2cba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80a3d1161ee347f55dbf9caff00c2b6",
+    "url": "/moviapp-redux/index.html"
+  },
+  {
+    "revision": "c3bb1aeba75be1a122b3",
+    "url": "/moviapp-redux/static/css/main.3ba39aa0.chunk.css"
+  },
+  {
+    "revision": "ad4776704a854750f576",
+    "url": "/moviapp-redux/static/js/2.6eb4e6f8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/moviapp-redux/static/js/2.6eb4e6f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3bb1aeba75be1a122b3",
+    "url": "/moviapp-redux/static/js/main.f69b3eea.chunk.js"
+  },
+  {
+    "revision": "3f11d1a2a59562f12a1b",
+    "url": "/moviapp-redux/static/js/runtime-main.93abeb93.js"
+  }
+]);
